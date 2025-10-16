c783b673d3f35b1bdff79ea06606360316c5959d
Bogdan <68660431+pozdnyakovbogdan00@users.noreply.github.com>
2025-10-16 16:24:03 +0500

    Add data models: LocationModel and PersonModel

---
+++ lib/feature/data/models/location_model.dart
@@ -0,0 +1,20 @@
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+
+class LocationModel extends LocationEntity
+{
+  const LocationModel({required name, required url}) : super(name: name, url: url);
+
+  factory LocationModel.fromJson(Map<String, dynamic> json) {
+    return LocationModel(
+      name: json['name'],
+      url: json['url'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'url': url,
+    };
+  }
+}
